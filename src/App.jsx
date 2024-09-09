b0e9b3ff322e495bcdcc473d264bd9e553eefcc9
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-09-09 17:59:38 +0500

    App.jsx configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
+import { BrowserRouter as Router, Switch, Route  } from "react-router-dom"
 
 function App() {
+
+  const [token, setToken] = useState(window.localStorage.getItem('token'));
+
   return (
     <>
-      
+      <Router>
+        <Switch>
+          { token ? nimadur : nimadur }
+        </Switch>
+      </Router>
     </>
   )
 }
