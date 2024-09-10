2ed2f577d0c963cc992dbc5bd3c6afb8f7b78f00
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-09-10 08:48:13 +0500

    fix Login page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
 
             <div className="mb-5">
               <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Your Password</label>
-              <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+              <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="************" required />
             </div>
 
             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-[360px] px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
